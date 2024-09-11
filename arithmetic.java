1913a2b163d58a6dfd653a91c0d7b6987415423d
MuyombaAmuza <amuzamuyomba@gmail.com>
2024-09-12 01:16:42 +0300

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,27 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=10;
+		int b=5;
+		
+		//adddition
+		int sum=a+b;
+		
+		//subtraction
+		int difference=a-b;
+		
+		//multiplication
+		int product=a*b;
+		
+		//division
+		int quotient=a/b;
+		
+		//modulus
+		int remainder=a%b;
+		
+	System.out.println("Sum=" +sum);
+	System.out.println("Subtraction=" +difference);
+	System.out.println("Product=" +product);
+	System.out.println("Quotient=" +quotient);
+	System.out.println("Remainder=" +remainder);
+	}
+}
