3b1bf1d304d3ae3f346443c4cc39772403e6072d
MuyombaAmuza <amuzamuyomba@gmail.com>
2024-09-12 01:18:14 +0300

    Add files via upload

---
+++ inputing.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+
+		//asking for user name		
+		System.out.println("Enter your name");
+		String name=sc.nextLine();
+		
+		//asking for user age
+		System.out.println("Enter your age");
+		String age=sc.nextLine();
+		
+		//asking for user fovourite food
+		System.out.println("Enter your favourite food");
+		String food=sc.nextLine();
+		
+		//asking for user favourite book 
+		System.out.println("Enter your book");
+		String book=sc.nextLine();
+	
+	}
+}
